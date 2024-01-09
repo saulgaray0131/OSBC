e57d99d60533441ccc0cce6cdfebae6833cb2998
SG <saulgaray0131@gmail.com>
2024-01-08 20:22:37 -0600

    f

--- src/model/osrs/construction.py
+++ src/model/osrs/construction.py
@@ -122,7 +122,7 @@ class OSRSConstruction(OSRSBot):
         while ocr.find_text("Creation", self.win.game_view, ocr.BOLD_12, [clr.ORANGE, clr.YELLOW]) == None:
             self.log_msg("Finding creation")
             time.sleep(.05)
-        time.sleep(rd.fancy_normal_sample(.1, .3))
+        time.sleep(rd.fancy_normal_sample(.3, .4))
         self.pressKey("6")
         time.sleep(rd.fancy_normal_sample(.3, .4))
         self.log_msg("12")
